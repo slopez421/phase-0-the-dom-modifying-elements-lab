5b6e5c12d0d41e19645ff9f3422d527e6cee2b37
Shania Lopez <slopez@fandm.edu>
2023-03-24 21:31:49 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+const body = document.querySelector("body");
+body.appendChild(newHeader);
+
+newHeader.textContent = "Shania is the champion";
+
